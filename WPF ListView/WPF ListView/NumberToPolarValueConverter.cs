527f20af7714fafd6af72b1b5283d9d85439d7d0
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-05-25 13:10:11 -0700

    Added DataTrigger

---
+++ WPF ListView/WPF ListView/NumberToPolarValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace WPF_ListView
+{
+    [ValueConversion(typeof(object), typeof(int))]
+    public class NumberToPolarValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int num = (int)System.Convert.ChangeType(value, typeof(int));
+            if (num == 0) return 0;
+            return num > 0 ? +1 : -1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("ConvertBack not supported");
+        }
+    }
+}
