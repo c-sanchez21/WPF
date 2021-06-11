bc3ba9bb9be83dab3c990d093b238b6c63f9b926
Cesar Sanchez <c_sanchez21@hotmail.com>
2021-06-11 08:23:56 -0700

    Added INotify

--- WPF DateTimePicker/WPF DateTimePicker/MainWindow.xaml.cs
+++ WPF DateTimePicker/WPF DateTimePicker/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,19 +21,49 @@ namespace WPF_DateTimePicker
     /// Interaction logic for MainWindow.xaml
     /// Solution to experiment with Date/Time Controls
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public DateTime MyDate { get; set; } = new DateTime(2021, 3, 21,12,30,0);
+        DateTime myDate = new DateTime(2021, 3, 21, 12, 30, 0);
+        public DateTime MyDate
+        {
+            get
+            {
+                return myDate;
+            }
+            set
+            {
+                myDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public DateTime? SelectedDate
+        {
+            get
+            {
+                return dtpDate.SelectedDate;
+            }
+            set
+            {
+                dtpDate.SelectedDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
 
         }
-
         private void btnDisplayDate_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(String.Format("MyDate:= {0:yyyy-MMM-dd ddd, HH:mm:ss}", MyDate));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
