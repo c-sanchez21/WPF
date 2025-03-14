8e2593451b88a3773edb7557163d2676c7431346
Cesar <c_sanchez21@hotmail.com>
2025-03-13 23:34:41 -0700

    Added a Timespan with Multibinding

--- WPF Timer/WPF Timer/MainWindow.xaml.cs
+++ WPF Timer/WPF Timer/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,12 +21,32 @@ namespace WPF_Timer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        TimeSpan duration; 
+        public TimeSpan Duration 
+        {
+            get { return duration; }
+            set 
+            { 
+                duration = value;
+                NotifyPropertyChanged();
+            }
+        }
+        DateTime StartTime = DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
             InitTimer();
+            this.DataContext = this;
         }
 
         private void InitTimer()
@@ -38,6 +60,7 @@ namespace WPF_Timer
         private void Timer_Tick(object sender, EventArgs e)
         {
             lblTimer.Content = DateTime.Now.ToString("HH:mm:ss");
+            Duration = DateTime.Now - StartTime;            
         }
     }
 }
